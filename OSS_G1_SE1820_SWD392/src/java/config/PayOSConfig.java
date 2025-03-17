4cd830435a397d35a51de3eed1bf281bd2d092a8
dunglove <dungdoile1@gmail.com>
2025-03-17 12:53:24 +0700

    Dung update PayOS ver1

--- OSS_G1_SE1820_SWD392/src/java/config/PayOSConfig.java
+++ OSS_G1_SE1820_SWD392/src/java/config/PayOSConfig.java
@@ -9,7 +9,11 @@ package config;
  * @author asus
  */
 public class PayOSConfig {
-    public static final String PAYOS_URL = "https://payos.vn/api/v1/payment";
-    public static final String PAYOS_API_KEY = "7b036656-2475-4221-bffd-87accb2e15f1"; // Thay bằng API Key thật của bạn
-    public static final String PAYOS_RETURN_URL = "http://localhost:8080/OSS_G1_SE1820_SWD392/payos_return";
+
+    public static final String CLIENT_ID = "21659637-2e1f-4597-a762-24f596cbcacc";
+    public static final String API_KEY = "7b036656-2475-4221-bffd-87accb2e15f1";
+    public static final String CHECKSUM_KEY = "34888fb5500ab22d3b44063e1752a92dd5e16540693839adc1f222cd211bc5c7";
+    public static final String RETURN_URL = "http://localhost:8080/your-app/success";
+    public static final String CANCEL_URL = "http://localhost:8080/your-app/cancel";
+    public static final String PAYOS_API_URL = "https://api.payos.vn";
 }
