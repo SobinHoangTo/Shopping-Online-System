85f20291f892004372bfd0fc251f0d0a48fac045
Vinh Vũ <vdqvinh2004@gmail.com>
2025-03-17 13:27:39 +0700

    Revert "Merge branch 'hoangnm' into 'develop'"
    This reverts merge request !7

--- OSS_G1_SE1820_SWD392/src/java/models/Entities/User.java
+++ OSS_G1_SE1820_SWD392/src/java/models/Entities/User.java
@@ -15,13 +15,6 @@ public class User {
         this.role = role;
     }
 
-    public User(String email, String password, String fullname, String role) {
-        this.email = email;
-        this.password = password;
-        this.fullname = fullname;
-        this.role = role;
-    }
-
     public int getId() {
         return id;
     }
